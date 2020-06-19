ff163d3c367153f78f2b2de099309287f5cc8ffd
Sylvain <sylvain.frappez@gmail.com>
2020-06-19 14:06:20 +0200

    {INIT} Project

---
+++ Assets/Scripts/FirstLoad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using System.Threading.Tasks;
+
+
+
+public class FirstLoad : MonoBehaviour
+{
+    public AssetReference assetReference;
+    [SerializeField] private string _label;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Get(_label);
+    }
+    
+    private async void Get(string _label)
+    {
+         Debug.Log("Get");
+        var locations = await Addressables.LoadResourceLocationsAsync(_label).Task;
+        foreach (var location in locations)
+        {
+            Debug.Log("Load");
+            await Addressables.InstantiateAsync(location).Task;
+        }
+    }
+
+}
---
+++ Assets/Scripts/LoadCounting.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class LoadCounting : MonoBehaviour
+{
+  [SerializeField] private Transform spawnAnchor = null;
+  [SerializeField] private float separation = 1f;
+  [SerializeField] private int instanceCount = 10;
+  [SerializeField] private AssetReference prefabReference = null;
+  private readonly List<GameObject> _instances = new List<GameObject>();
+  public void HandleLifecycle()
+  {
+    var hasSpawnedInstances = _instances.Count > 0; 
+    if (hasSpawnedInstances)
+      Despawn();
+    else
+      Spawn();
+  }
+  private void Spawn()
+  {
+    for (var i = 0; i < instanceCount; i++)
+    {
+      var asyncOperationHandle = prefabReference.InstantiateAsync(spawnAnchor.position + i *separation * Vector3.right, spawnAnchor.rotation);
+      asyncOperationHandle.Completed += handle => { _instances.Add(handle.Result); };
+    }
+  }
+  private void Despawn()
+  {
+    foreach (var instance in _instances)
+      Addressables.ReleaseInstance(instance);
+    _instances.Clear();
+  }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
